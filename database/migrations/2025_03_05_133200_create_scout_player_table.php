470222dc4718a56495382d85f19e8b12676309e0
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-03-05 10:43:08 -0300

    database configuration finished

---
+++ database/migrations/2025_03_05_133200_create_scout_player_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scout_player', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->unsignedBigInteger('player_id');
+            $table->unsignedBigInteger('scout_id');
+            $table->foreign('player_id')
+                ->references('id')
+                ->on('players');
+            $table->foreign('scout_id')
+                ->references('id')
+                ->on('scouts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scout_player');
+    }
+};
