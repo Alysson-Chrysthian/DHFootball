fe66e9387798cbdb4bb1bd24b6745ae8043201a6
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-03-15 12:03:27 -0300

    scout profile frontend done

--- resources/views/livewire/page/scout/profile.blade.php
+++ resources/views/livewire/page/scout/profile.blade.php
@@ -1,3 +1,38 @@
+@pushOnce('styles')
+    @vite('resources/css/pages/profile.css')
+@endPushOnce
+
 <div>
-    {{-- In work, do what you enjoy. --}}
+
+    <form>
+        <div>
+            <x-image-input />
+        </div>
+
+        <div>
+            <x-edit-text-input 
+                label="Nome"
+                placeholder="Seu novo nome"
+                id="name-input"
+            />
+
+            <x-edit-text-input 
+                label="Email"
+                placeholder="Seu novo email"
+                id="email-input"
+            />
+
+            <x-edit-text-input 
+                label="Senha"
+                placeholder="Sua nova senha"
+                id="password-input"
+            />
+
+            <x-select
+                label="Club"
+                id="club-input"
+            ></x-select>
+        </div>
+    </form>
+
 </div>
