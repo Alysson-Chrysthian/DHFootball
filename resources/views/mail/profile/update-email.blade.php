c6cd81bea467800d7d4c6e7914a36eb9396f0c64
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-03-17 20:24:20 -0300

    preparations for email update done

---
+++ resources/views/mail/profile/update-email.blade.php
@@ -0,0 +1,11 @@
+<x-mail::message>
+Se você esta tentando mudar o email na sua conta do dhfootball para esse,<br>
+entao por favor, clique no link abaixo para confirmar a mudança
+
+<x-mail::button :url="''">
+Atualizar email
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
